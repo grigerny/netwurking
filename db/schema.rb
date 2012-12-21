e25b8cf5c3f0559ba25668d9a9362def833f354a
Gary Riger <grigerny@aol.com>
2012-12-21 14:50:50 -0500

    added address fields to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121217034109) do
+ActiveRecord::Schema.define(:version => 20121221195015) do
 
   create_table "bizcards", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -55,6 +55,12 @@ ActiveRecord::Schema.define(:version => 20121217034109) do
     t.string   "twitter"
     t.string   "cardbackgroundimage"
     t.string   "token"
+    t.string   "address"
+    t.string   "address2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "country"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
